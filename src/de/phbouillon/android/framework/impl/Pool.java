e9f800c0ef3e26f6a9e0b0c25e999d8504d75e31
merkinth <merkinth@gmail.com>
2019-01-20 20:48:18 +0100

    Fixed concurrent modification (changed Set to List, removed iterator, synchronized list modifier methods).

--- src/de/phbouillon/android/framework/impl/Pool.java
+++ src/de/phbouillon/android/framework/impl/Pool.java
@@ -2,7 +2,7 @@ package de.phbouillon.android.framework.impl;
 
 /* Alite - Discover the Universe on your Favorite Android Device
  * Copyright (C) 2015 Philipp Bouillon
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License, or
@@ -21,8 +21,8 @@ package de.phbouillon.android.framework.impl;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import de.phbouillon.android.games.alite.AliteLog;
 
@@ -30,50 +30,44 @@ public class Pool <T extends Serializable> implements Serializable {
 	private static final long serialVersionUID = -5141492387845777888L;
 
 	public interface PoolObjectFactory <T extends Serializable> extends Serializable {
-		public T createObject();
+		T createObject();
 	}
-	
-	private final Set <T> freeObjects;
+
+	private final List<T> freeObjects;
 	private final PoolObjectFactory <T> factory;
 	private final int maxSize;
-	
+
 	public Pool(PoolObjectFactory <T> factory, int maxSize) {
 		this.factory = factory;
 		this.maxSize = maxSize;
-		this.freeObjects = new HashSet<T>(maxSize);
+		freeObjects = new ArrayList<>(maxSize);
 	}
-	
-	private void writeObject(ObjectOutputStream out)
-            throws IOException {
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
 		try {
 			out.defaultWriteObject();
 		} catch(IOException e) {
 			AliteLog.e("PersistenceException", "Pool " + getClass().getName(), e);
-			throw(e);
+			throw e;
 		}
-    }
+	}
 
-	public T newObject() {
-		T object = null;
-		
+	public synchronized T newObject() {
 		if (freeObjects.isEmpty()) {
-			object = factory.createObject();
-		} else {
-			object = freeObjects.iterator().next(); 
-			freeObjects.remove(object);
+			return factory.createObject();
 		}
-		
+		T object = freeObjects.get(0);
+		freeObjects.remove(0);
 		return object;
 	}
-	
+
 	public void free(T object) {
 		if (freeObjects.size() < maxSize) {
 			freeObjects.add(object);
 		}
 	}
-	
-	public void reset() {
+
+	public synchronized void reset() {
 		freeObjects.clear();
 	}
 }
- 
